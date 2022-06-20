7e1d72351edf49893baa789e08e40bfda588dba9
unknown <abhishektk333@gmail.com>
2022-06-20 07:33:29 +0530

    next change

--- src/Book.js
+++ src/Book.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams} from 'react-router-dom';
 import games from "./img/games.png"
+import crime from "./img/crime.jpg"
 import "./crime.css"
 
 
@@ -9,21 +10,23 @@ const Book = (props)=>{
     const genre = useParams()
     console.log(genre)
     if(genre.bookId === "crimebook"){
-        return (
-            
-            <div className="book">
+        return (  
+            <div className="book" style={{
+                "&::before": {backgroundImage:`url(${crime})`}
+            }}>
                 <div className="crime">
                         <div className="Game">
                     <img src={games} alt="game"/>
                 </div>
                 <div className="Book">
                     <p id="side-cover">FACTS ON</p>
-                    <div className="cover"></div>
+                    <div className="cover" style={{
+                        backgroundImage:`url(${crime})`
+                    }}></div>
                     <h2>CRIME</h2>
                     <span className="writer">Designed By <i>Sanblaze</i></span>
                 </div>
             </div>
-    
             </div>
       
         )
