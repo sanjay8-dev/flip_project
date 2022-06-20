09f67807d36700b7dc2b59f67dae05a8132359d7
Sanjay Prakash <sanjayprakash99@gmail.com>
2022-06-20 09:30:32 +0530

    side cover changes

--- src/Book.js
+++ src/Book.js
@@ -7,18 +7,22 @@ import "./crime.css"
 
 
 const Book = (props)=>{
+    console.log(crime)
+    const testStyle = {
+        "--backgroundImage" :`url(${crime})`,
+    }
     const genre = useParams()
     console.log(genre)
     if(genre.bookId === "crimebook"){
         return (  
             <div className="book" style={{
-                "&::before": {backgroundImage:`url(${crime})`}
+                
             }}>
                 <div className="crime">
                         <div className="Game">
                     <img src={games} alt="game"/>
                 </div>
-                <div className="Book">
+                <div className="Book" style={testStyle}>
                     <p id="side-cover">FACTS ON</p>
                     <div className="cover" style={{
                         backgroundImage:`url(${crime})`
