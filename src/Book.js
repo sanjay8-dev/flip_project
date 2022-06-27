75ebcd0811411ddd2db72663a75317a0761819ce
unknown <abhishektk333@gmail.com>
2022-06-27 12:54:21 +0530

    commit after adding books

--- src/Book.js
+++ src/Book.js
@@ -2,21 +2,24 @@ import React from 'react'
 import { useParams} from 'react-router-dom';
 import games from "./img/games.png"
 import crime from "./img/crime.jpg"
-import "./crime.css"
+import motivation from "./img/motivation.jpg"
+import mystery from "./img/mystery.jpg"
+import "./book.css"
 
 
 
 const Book = (props)=>{
     console.log(crime)
-    const testStyle = {
-        "--backgroundImage" :`url(${crime})`
-    }
+    
     const genre = useParams()
     console.log(genre)
     if(genre.bookId === "crimebook"){
+        const testStyle = {
+            "--backgroundImage" :`url(${crime})`
+        }
         return (  
             <div className="book" style={testStyle}>
-                <div className="crime">
+                <div className="booksonshelf">
                         <div className="Game">
                     <img src={games} alt="game"/>
                 </div>
@@ -26,18 +29,55 @@ const Book = (props)=>{
                         backgroundImage:`url(${crime})`
                     }}></div>
                     <h2>CRIME</h2>
-                    <span className="writer">Designed By <i>Sanblaze</i></span>
+                    <span className="writer">Written By <i>Sanblaze</i></span>
                 </div>
             </div>
             </div>
-      
         )
-
     }
-    else{
-        return <div>"Nt working"</div>
+    else if(genre.bookId === "motivationbook"){
+        const testStyle = {
+            "--backgroundImage" :`url(${motivation})`
+        }
+        return (
+            <div className="book" style={testStyle}>
+            <div className="booksonshelf">
+                    <div className="Game">
+                <img src={games} alt="game"/>
+            </div>
+            <div className="Book" style={testStyle}>
+                <p id="side-cover">FACTS ON</p>
+                <div className="cover" style={{
+                    backgroundImage:`url(${motivation})`
+                }}></div>
+                <h2>MOTIVATION</h2>
+                <span className="writer">Written By <i>Astroboy</i></span>
+            </div>
+        </div>
+        </div>
+        )
+    }else if(genre.bookId === "mysterybook"){
+        const testStyle = {
+            "--backgroundImage" :`url(${mystery})`
+        }
+        return (
+            <div className="book" style={testStyle}>
+            <div className="booksonshelf">
+                    <div className="Game">
+                <img src={games} alt="game"/>
+            </div>
+            <div className="Book" style={testStyle}>
+                <p id="side-cover">FACTS ON</p>
+                <div className="cover" style={{
+                    backgroundImage:`url(${mystery})`
+                }}></div>
+                <h2>MYSTERY</h2>
+                <span className="writer">Written By <i>El_fuego</i></span>
+            </div>
+        </div>
+        </div>
+        )
     }
-    
 }
 
 export default Book
--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,29 @@
 import"./Home.css";
-import {Link, animateScroll as scroll} from "react-scroll"
+import { Link , animateScroll as scroll } from "react-scroll"
+import { useNavigate } from "react-router-dom"; 
 import shelfImg from "./img/shelf.png"
 import scrollupImg from "./img/scrollup.png"
 import React, { useState, useEffect, useRef } from 'react'
-// import BIRDS from 'vanta/dist/vanta.birds.min'
 import NET from 'vanta/dist/vanta.net.min'
 import * as THREE from 'three'
 
 const Home = (props) => {
 const [vantaEffect, setVantaEffect] = useState(0)
 const myRef = useRef(null)
+
+const navigate = useNavigate();
+const navigateToCrimeBook = () => {
+    navigate('/book/crimebook');
+  };
+
+  const navigateToMotivationBook = () => {
+    navigate('/book/motivationbook');
+  };
+
+  const navigateToMysteryBook = () => {
+    navigate('/book/mysterybook');
+  };
+
 useEffect(() => {
 if (!vantaEffect) {
 setVantaEffect(NET({
@@ -29,13 +43,15 @@ if (vantaEffect) vantaEffect.destroy()
 }
 }, [vantaEffect])
 
+
 useEffect(()=>{
     scrollToTop();
-
 })
 const scrollToTop = () => {
     scroll.scrollToTop(); 
 };
+
+
 return (<div ref={myRef}>
   <div className= "land_container">
 	
@@ -45,7 +61,7 @@ return (<div ref={myRef}>
 				<h3 className="first_header">Think Before You Speak</h3>
 				<h3 className="second_header">Read Before You Think</h3>
 				<p className="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-        <Link to="finish" smooth ={true}  className="explore">Let's Flip</Link>
+        <Link to="finish" smooth ={true} className="explore">Let's Flip</Link>
 			</div>
 		</div>
 	</div>
@@ -54,21 +70,21 @@ return (<div ref={myRef}>
         <div className="container">
             <div className="shelf">
 
-                <div className="CrimeBook">
-                        <span className="writer">Designed By <i>Sanblaze</i></span>
+                <div className="CrimeBook" onClick={navigateToCrimeBook}>
+                        <span className="writer">Written By <i>Sanblaze</i></span>
                         <h2>CRIME</h2>
                 </div>
 
                 <img src={shelfImg} alt="shelf"/>
                 <br/>
                 
-                <div className="MysteryBook">
-                    <span className="writer">Designed By <i>Aveedan</i></span>
+                <div className="MysteryBook" onClick={navigateToMysteryBook}>
+                    <span className="writer">Written By <i>Astroboy</i></span>
                     <h2>MYSTERY</h2>
                 </div>
 
-                <div className="MotivationBook">
-                    <span className="writer">Designed By <i>El_fuego</i></span>
+                <div className="MotivationBook" onClick={navigateToMotivationBook}>
+                    <span className="writer">Written By <i>El_fuego</i></span>
                     <h2>MOTIVATION</h2>
                 </div>
                 <img src={shelfImg} alt= "shelf" className="special"/>
